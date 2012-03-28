be1b2382543dd90ccfd54deb8434a6e74695f435
GaryS <garethjscott@gmail.com>
2012-03-28 06:58:37 -0700

    Successful compile of interpret.exe. But, needs to be implemented!

---
+++ interpret.cpp
@@ -0,0 +1,351 @@
+/*
+	Copyright (c) Gareth Scott 2012
+
+	interpret.cpp 
+
+    interpretIndex = 0;
+    while (interpretIndex <= interpretEnd) {
+        switch (program[interpretIndex].type) {
+        case operand: {
+            evalStack.push(program[interpretIndex].value);
+            break;
+        case operator:
+            // A binary operator will pop the 2 operands off the stack,
+            //  evaluate and push the result back.
+            // For short-circuit optimization, look up the program for the
+            //  operator at the next highest level and if it's || and the
+            //  current evaluation is TRUE then move the interpretIndex just
+            //  past that || operator.
+            evaluate(program[interpretIndex].value);
+            break;
+        default:
+            assert(false);
+            break;
+        }
+    }
+    
+*/
+
+#include "interpret.h"
+//#include "lmi_timer.h"
+//#include <math.h>
+#include <assert.h>
+
+interpret::interpret() :
+#if CYGWIN 
+					logc(std::string("INTERPRET"))
+#endif /* CYGWIN */					
+					//_positionCurrent(0), _directionPositive(true), _timerRunning(false), _superState(IDLE) {
+                    {
+}
+
+#if 0
+void stepper_init(void) {
+    GPIODirModeSet(GPIO_PORTA_BASE, PIN_ALL, GPIO_DIR_MODE_OUT);
+    GPIOPadConfigSet(GPIO_PORTA_BASE, PIN_ALL, GPIO_STRENGTH_8MA, GPIO_PIN_TYPE_STD);
+}
+
+void stepper::test(void) {
+#if CYGWIN
+	oss() << "moveAbsolute START" << endl;
+	moveAbsolute(2000);
+	oss() << "_positionCurrent=" << _positionCurrent <<
+			" _positionConstantVelocityStart=" << _positionConstantVelocityStart <<
+			" _positionConstantVelocityEnd=" << _positionConstantVelocityEnd <<
+			" _positionTarget=" << _positionTarget << endl;
+	oss() << "moveAbsolute END";
+	dump();
+	// Now let's run the isr.
+	for (int i = 0; i < 2010 && _superState != IDLE; ++i) {
+		isr();
+	}
+	
+	oss() << endl << "moveAbsolute START" << endl;
+	moveAbsolute(0);
+	oss() << "_positionCurrent=" << _positionCurrent <<
+			" _positionConstantVelocityStart=" << _positionConstantVelocityStart <<
+			" _positionConstantVelocityEnd=" << _positionConstantVelocityEnd <<
+			" _positionTarget=" << _positionTarget << endl;
+	oss() << "moveAbsolute END";
+	dump();
+	for (int i = 0; i < 2010 && _superState != IDLE; ++i) {
+		isr();
+	}
+
+	// velocity move
+	oss() << endl << "velocity START" << endl;
+	velocity(800);
+	oss() << "_positionCurrent=" << _positionCurrent <<
+			" _positionConstantVelocityStart=" << _positionConstantVelocityStart << endl;
+	oss() << "velocity END";
+	dump();
+	for (int i = 0; i < 2000; ++i) {
+		isr();
+	}
+#endif /* CYGWIN */
+}
+
+// Set motor to run continuously at a given velocity. To stop, set velocity to 0.
+//  This function cannot be used until moveAbsolute() has completed.
+int stepper::velocity(const int f) {
+	if (_superState == IDLE) {
+		// Starting from f = 0.
+		if (f == 0) {
+			return SUCCESS;	// nothing to do
+        } else if (f > 0) {
+            directionPositive();
+            a.frequency(a.fmin(), f);
+        } else {
+            // f < 0
+            directionPositive(false);
+            a.frequency(a.fmin(), -f);
+        }
+		_updateConstantVelocityStart();
+        _superState = VELOCITY_MOVE;
+		a.primeTime(1000000);
+	} else if (_superState == VELOCITY_MOVE_ACCELERATE || _superState == VELOCITY_MOVE_DECELERATE) {
+		// Can't do it right now. Try again when we're done accelerating.
+	} else {
+        // motor already running at a constant velocity
+        assert(_superState == VELOCITY_MOVE);
+        if ((f > 0 && !_directionPositive) || (f < 0 && _directionPositive)) {
+            // Trying to change direction. Can't do this. Instead, bring motor to a stop and then issue another velocity() command to
+            //  reverse direction.
+            return ILLEGAL_VELOCITY;
+        }
+		int fdiff = f - a.freqFromClockTicks(_timerPeriod);
+		if (fdiff == 0) {
+			// nothing to do
+			assert(_subState == VELOCITY_MOVE_CONSTANT_VELOCITY);
+			return SUCCESS;
+		} else if (fdiff > 0) {
+			_subState = VELOCITY_MOVE_ACCELERATE;
+			a.frequency(a.freqFromClockTicks(_timerPeriod), f);
+		} else {
+			// fdiff < 0
+			_subState = VELOCITY_MOVE_DECELERATE;
+			a.frequency(f, a.freqFromClockTicks(_timerPeriod));
+		}
+		_updateConstantVelocityStart();
+	}
+	_timerStart();
+    return SUCCESS;
+}
+
+void stepper::_updateConstantVelocityStart(void) {
+	unsigned int accelStep = a.timeToSteps(a.time());
+	if (_directionPositive) {
+		_positionConstantVelocityStart = _positionCurrent + accelStep;;
+	} else {
+		_positionConstantVelocityStart = _positionCurrent - accelStep;;
+	}
+	_subState = VELOCITY_MOVE_ACCELERATE;
+}
+
+// Set motor to run continuously at a given velocity. To stop, set velocity to 0.
+//  This function cannot be used until velocity() has brought motor to a halt.
+void stepper::moveAbsolute(int positionNew) {
+	if (_superState != IDLE) {
+		// Can't do it right now. Try again when we're done accelerating.
+		return;
+	}
+	unsigned int positionDelta;
+	_positionTarget = positionNew;
+	if (_positionTarget > _positionCurrent) {
+		directionPositive();
+		positionDelta = _positionTarget - _positionCurrent;
+	} else {
+		directionPositive(false);
+		positionDelta = _positionCurrent - _positionTarget;
+	}
+	// 0. Initialize timer
+	// 1. Start timer
+	// 2. Ramp up through acceleration curve
+	// 3. Maintain max speed until time to decelerate
+	// 4. Ramp down acceleration curve (run acceleration curve backwards)
+	// 5. Stop timer.
+	
+	// 1. Set acceleration time (e.g. 0.5s)
+	unsigned int accelStep = a.timeToSteps(a.time());
+#if REGRESS_1
+	oss() << "moveAbsolute() accelStep=" << accelStep << endl;
+#endif /* REGRESS_1 */
+	if (positionDelta > 2 * accelStep) {
+		// Enough room for full acceleration profile
+		if (_directionPositive) {
+			_positionConstantVelocityStart = _positionCurrent + accelStep;
+			_positionConstantVelocityEnd = _positionTarget - accelStep;
+		} else {
+			_positionConstantVelocityStart = _positionCurrent - accelStep;
+			_positionConstantVelocityEnd = _positionTarget + accelStep;
+		}
+		_superState = MOVE_FULL;
+		a.primeTime(1000000);
+	} else {
+		// Acceleration curve needs to be truncated. Not enough room to reach max speed.
+		unsigned int tNew = a.stepsToTime(positionDelta / 2);
+		// Set fMAX and then calculate time required for acceleration.
+		unsigned int us = a.microSecToCurveIndex(tNew);
+		_fminOld = a.fmin();
+		_fmaxOld = a.fmax();
+		unsigned int fmax = a.freqFromTime(us);
+		a.frequency(_fminOld, fmax);	// Rebuilds acceleration tables. Put them back when this move is finished.
+		// Set truncated acceleration time.
+		a.primeTime(tNew);
+		// Remember to rebuild accel tables when move is over. Set trigger to do this in isr().
+		if (_directionPositive) {
+			_positionConstantVelocityStart = _positionConstantVelocityEnd = _positionCurrent + positionDelta / 2;
+		} else {
+			_positionConstantVelocityStart = _positionConstantVelocityEnd = _positionCurrent - positionDelta / 2;
+		}
+		_superState = MOVE_TRUNCATED;
+	}
+	_subState = MOVE_START;
+	_timerStart();
+}
+
+// Called every time timer times out.
+void stepper::isr(void) {
+	step();
+	if (_superState == MOVE_FULL || _superState == MOVE_TRUNCATED) {
+		if (_directionPositive) {
+			if (_positionCurrent < _positionConstantVelocityStart) {
+				// accelerating
+				_subState = MOVE_ACCELERATE;
+				_timer(a.updateClockPeriod());
+			} else if (_positionCurrent < _positionConstantVelocityEnd) {
+				// constant velocity. Nothing to do.
+				_subState = MOVE_CONSTANT_VELOCITY;
+			} else if (_positionCurrent == _positionConstantVelocityEnd) {
+				// Start of deceleration.
+				_subState = MOVE_DECELERATE;
+				a.primeTime(a.time());
+			} else if (_positionCurrent < _positionTarget) {
+				// decelerating
+				_subState = MOVE_DECELERATE;
+				_timer(a.updateClockPeriodReverse());
+			} else /* if (_positionCurrent >= _positionTarget) */ {
+				// end of movement
+				_timerStart(false);
+			}
+		} else {
+			if (_positionCurrent > _positionConstantVelocityStart) {
+				// accelerating
+				_subState = MOVE_ACCELERATE;
+				_timer(a.updateClockPeriod());
+			} else if (_positionCurrent > _positionConstantVelocityEnd) {
+				// constant velocity. Nothing to do.
+				_subState = MOVE_CONSTANT_VELOCITY;
+			} else if (_positionCurrent == _positionConstantVelocityEnd) {
+				// Start of deceleration.
+				_subState = MOVE_DECELERATE;
+				a.primeTime(a.time());
+			} else if (_positionCurrent > _positionTarget) {
+				// decelerating
+				_subState = MOVE_DECELERATE;
+				_timer(a.updateClockPeriodReverse());
+			} else /* if (_positionCurrent <= _positionTarget) */ {
+				// end of movement
+				_timerStart(false);
+			}
+		}
+	} else if (_superState == VELOCITY_MOVE) {
+		if (_directionPositive) {
+			if (_positionCurrent < _positionConstantVelocityStart) {
+				// accelerating
+				_timer(a.updateClockPeriod());
+			} else if (_positionCurrent == _positionConstantVelocityStart) {
+				if (a.freqCloseToStop(a.freqFromClockTicks(_timer()))) {
+					// end of movement
+					_timerStart(false);
+				} else {
+					_subState = VELOCITY_MOVE_CONSTANT_VELOCITY;
+				}
+			// } else if (_positionCurrent >= _positionConstantVelocityStart) { // compiler could optimize this line out, but we'll do it just to be explicit
+				// constant velocity. Nothing to do.
+			}
+		} else {
+			if (_positionCurrent > _positionConstantVelocityStart) {
+				// accelerating
+				_timer(a.updateClockPeriod());
+			} else if (_positionCurrent == _positionConstantVelocityStart) {
+				if (a.freqCloseToStop(a.freqFromClockTicks(_timer()))) {
+					// end of movement
+					_timerStart(false);
+				} else {
+					_subState = VELOCITY_MOVE_CONSTANT_VELOCITY;
+				}
+			// } else if (_positionCurrent <= _positionConstantVelocityStart) {
+				// constant velocity. Nothing to do.
+			}
+		}
+	}
+#if REGRESS_1
+	switch (_superState) {
+	case IDLE:
+		oss() << "IDLE ";
+		break;
+	case MOVE_FULL:
+		oss() << "MOVE_FULL ";
+		break;
+	case MOVE_TRUNCATED:
+		oss() << "MOVE_TRUNCATED ";
+		break;
+	case VELOCITY_MOVE:
+		oss() << "VELOCITY_MOVE ";
+		break;
+	default:
+		oss() << "UNKNOWN SUPERSTATE ";
+		break;
+	}
+
+	switch (_subState) {
+	case MOVE_START:
+		oss() << "MOVE_START ";
+		break;
+	case MOVE_ACCELERATE:
+		oss() << "MOVE_ACCELERATE ";
+		break;
+	case MOVE_CONSTANT_VELOCITY:
+		oss() << "MOVE_CONSTANT_VELOCITY ";
+		break;
+	case MOVE_DECELERATE:
+		oss() << "MOVE_DECELERATE ";
+		break;
+	case VELOCITY_MOVE_ACCELERATE:
+		oss() << "VELOCITY_MOVE_ACCEL ";
+		break;
+	case VELOCITY_MOVE_DECELERATE:
+		oss() << "VELOCITY_MOVE_DECEL ";
+		break;
+	case VELOCITY_MOVE_CONSTANT_VELOCITY:
+		oss() << "VELOCITY_MOVE_CONSTANT_VELOCITY ";
+		break;
+	default:
+		oss() << "UNKNOWN SUBSTATE ";
+		break;
+	}
+	oss() << " position=" << _positionCurrent << " timer=" << _timer();
+	dump();
+#endif /* REGRESS_1 */				
+}
+
+void stepper::_timerStart(bool start /* = true */) {
+	if (start) {
+		_timerRunning = true;
+		TimerEnable(TIMER0_BASE, TIMER_A);
+	} else {
+		_timerRunning = false;
+		TimerDisable(TIMER0_BASE, TIMER_A);
+		if (_superState == MOVE_TRUNCATED) {
+			// Reset acceleration tables
+			a.frequency(_fminOld, _fmaxOld);
+		}
+		_superState = IDLE;
+	}
+}
+#endif
+
+int main(void) {
+    return 0;
+}
---
+++ interpret.h
@@ -0,0 +1,129 @@
+/*
+	Copyright (c) Gareth Scott 2012
+
+	interpret.h 
+
+*/
+
+#ifndef _INTERPRET_H_
+#define _INTERPRET_H_
+
+#if 0
+#include "hw_ints.h"
+#include "hw_memmap.h"
+#include "hw_types.h"
+#include "gpio.h"
+#include "lmi_timer.h"
+
+#include "accel.h"
+#endif
+
+#if CYGWIN
+//#include <iostream>
+#include "log.h"
+#include <sstream>
+
+using namespace std;
+#endif /* CYGWIN */
+
+#if 0
+#define OPTIMIZE_CURVE_CALC 1
+
+#define PIN_ENABLE  (GPIO_PIN_4)
+#define PIN_SLEEP   (GPIO_PIN_5)
+#define PIN_STEP    (GPIO_PIN_6)
+#define PIN_DIR     (GPIO_PIN_7)
+#define PIN_ALL     (PIN_ENABLE | PIN_SLEEP | PIN_STEP | PIN_DIR)
+#endif
+
+class interpret
+#if CYGWIN
+				: public logc
+#endif /* CYGWIN */
+				{
+public:
+    interpret();
+    ~interpret() {}
+
+#if 0    
+	accel a;
+	
+    void step(void) {
+        GPIOPinWrite(GPIO_PORTA_BASE, PIN_STEP, PIN_STEP);
+        if (_directionPositive) {
+            ++_positionCurrent;
+        } else {
+            --_positionCurrent;
+        }
+        GPIOPinWrite(GPIO_PORTA_BASE, PIN_STEP, ~PIN_STEP);
+    }
+      
+    void directionPositive(bool positive = true) {
+        _directionPositive = positive;
+        if (_directionPositive) {
+            GPIOPinWrite(GPIO_PORTA_BASE, PIN_DIR, (unsigned char)~PIN_DIR);
+        } else {
+            GPIOPinWrite(GPIO_PORTA_BASE, PIN_DIR, PIN_DIR);
+        }
+    }
+    //bool directionPositive(void) {return _directionPositive;}
+      
+	// Only allow one of these, moveAbsolute() or velocity(), to be active at any one time.  
+    void moveAbsolute(int positionNew);
+	int velocity(const int f);
+    void isr(void);
+    //int acceleration(void) {/* TODO */}
+    //int velocity(void) {/* TODO */}
+    //int position(void) {/* TODO */} // Need to return both degrees and scaled position.
+	
+	void test(void);
+    
+private:
+	// _superState
+    enum {IDLE, MOVE_FULL, MOVE_TRUNCATED, VELOCITY_MOVE};
+	
+	// _subState
+	enum {MOVE_START, MOVE_ACCELERATE, MOVE_CONSTANT_VELOCITY, MOVE_DECELERATE,
+			VELOCITY_MOVE_ACCELERATE, VELOCITY_MOVE_DECELERATE, VELOCITY_MOVE_CONSTANT_VELOCITY};
+
+    // return values
+    enum {SUCCESS, ILLEGAL_MOVE, ILLEGAL_VELOCITY};                            
+            
+    void _init(void);
+    
+private:
+	void _timerStart(bool start = true);
+	void _timer(unsigned long newClockPeriod) {
+		_timerPeriod = newClockPeriod;
+		TimerLoadSet(TIMER0_BASE, TIMER_A, _timerPeriod);
+	}
+	unsigned long _timer(void) {return _timerPeriod;}
+	void _updateConstantVelocityStart(void);
+	
+	// These 4 points represent a typical movement profile.
+	//  Acceleration is from _positionCurrent to _positionConstantVelocityStart
+	//  Constant velocity is from _positionConstantVelocityStart to _positionConstantVelocityEnd
+	//  Deceleration is from _positionConstantVelocityEnd to _positionTarget
+	// If, instead, a velocity() command is being issued, only 2 of the points are used.
+	//  Increasing velocity is from _positionCurrent to _positionConstantVelocityStart
+	//  Decreasing velocity is from _positionConstantVelocityEnd to _positionTarget
+	// To change direction, 2 velocity, or moveAbsolute, commands should be used. One to bring
+	//  the motor to 0 velocity and the next to move in the opposite direction.
+    int _positionCurrent;  // in steps
+	int _positionConstantVelocityStart;
+	int _positionConstantVelocityEnd;
+	int _positionTarget;
+	
+    bool _directionPositive;    // false -> direction is negative
+	bool _timerRunning;
+	unsigned long _timerPeriod;
+    //int _acceleration;          // 0 when constant velocity
+    //bool _accelerating; 
+    //int _velocity;              // step/s
+	unsigned int _fminOld, _fmaxOld;	// save frequency min/max for acceleration. Used when acceleration is trucated.
+	unsigned int _superState;
+	unsigned int _subState;
+#endif    
+};
+
+#endif /* _INTERPRET_H_ */
